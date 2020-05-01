6c5a17de7e89f7a5fcb9dc5d8c5b5200413e94c7
Amanoba <64486419+Amanoba@users.noreply.github.com>
2020-05-01 11:29:30 +0200

    add jquery first few examples

---
+++ scripts.js
@@ -0,0 +1,33 @@
+// Third part:
+
+$('h1').html('This is done by <em>JavaSript!</em>');
+$('p').css('border', '1px solid red');
+$('p').css('padding', '10px');
+$('h2').toggleClass('blue'); /*bekapcsolom*/
+$('h2').toggleClass('blue'); /*kikapcsolom*/
+$('h2').toggleClass('blue'); /* megint bekapcsolom*/
+console.log('Script file is loaded.');
+
+// Single line comment
+
+/*
+Multiple
+lines
+of comments
+*/
+
+// First part:
+
+// 1+1
+// 2
+// 4*3
+// 12
+// let age;
+// age = 48
+// 48
+// age + 5
+// 53
+// let numberOFLegs = 4
+// undefined
+// let name = Csaba
+//
